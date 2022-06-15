6e907536c264692cc16790f2ef63e25bb5d666ce
JoyAnneW <joyawilliams@gmail.com>
2022-06-15 20:27:35 +0200

    set up route using useParams to individual products.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./components/HomePage";
 import { Routes, Route } from "react-router-dom";
 import NotFound from "./components/NotFound";
 import Shop from "./components/Pages/Shop/Shop";
+import SingleProduct from "./components/SingleProduct/SingleProduct";
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 			<Routes>
 				<Route exact path="/" element={<HomePage />} />
 				<Route path="/shop" element={<Shop />} />
+				<Route path="/product/:id" element={<SingleProduct />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</div>
--- src/components/SingleProduct/SingleProduct.js
+++ src/components/SingleProduct/SingleProduct.js
@@ -3,6 +3,7 @@ import { ProductsContext } from "../../context/products-context";
 import { useParams, useNavigate } from "react-router-dom";
 import Layout from "../shared/Layout";
 import "./SingleProduct.styles.scss";
+import NotFound from "../NotFound";
 
 export default function SingleProduct() {
 	const { products } = useContext(ProductsContext);
@@ -11,6 +12,7 @@ export default function SingleProduct() {
 
 	// Get the id param from the URL.
 	const { id } = useParams();
+	console.log(id);
 
 	// on load check if the product id from params is found in the products list. Number()makes sure it's a number
 	useEffect(() => {
@@ -20,7 +22,60 @@ export default function SingleProduct() {
 		if (!product) {
 			navigate("/shop");
 		}
+
+		// if product is NotFound, set it as state
+		setProduct(product);
 	});
 
-	return <div>SingleProduct</div>;
+	// handling the case where product is null right when component loads for first time.
+	if (!product) {
+		return null;
+	}
+	const { imageUrl, title, price, description } = product;
+	const itemInCart = true;
+	return (
+		<Layout>
+			<div className="single-product-container">
+				<div className="product-image">
+					<img src={imageUrl} alt="product" />
+				</div>
+				<div className="product-details">
+					<div className="name-price">
+						<h3>{title}</h3>
+						<p>{price}</p>
+					</div>
+					<div className="add-to-cart-btns">
+						{!itemInCart && (
+							<button
+								className="button is-white nomad-btn"
+								id="btn-white-outline"
+								// onClick={() => addProduct(product)}
+							>
+								ADD TO CART
+							</button>
+						)}
+						{itemInCart && (
+							<button
+								className="button is-white nomad-btn"
+								id="btn-white-outline"
+								// onClick={() => increase(product)}
+							>
+								ADD MORE
+							</button>
+						)}
+
+						<button
+							className="button is-black nomad-btn"
+							id="btn-white-outline"
+						>
+							PROCEED TO CHECKOUT
+						</button>
+					</div>
+					<div className="product-description">
+						<p>{description}</p>
+					</div>
+				</div>
+			</div>
+		</Layout>
+	);
 }
